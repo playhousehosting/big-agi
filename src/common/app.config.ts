8b9eb0defc56b94274e97e83c8d27219109ccffa
playhousehosting <126509681+playhousehosting@users.noreply.github.com>
2024-04-07 02:49:27 -0700

    Update app.config.ts

--- src/common/app.config.ts
+++ src/common/app.config.ts
@@ -8,23 +8,23 @@
  */
 export const Brand = {
   Title: {
-    Base: 'big-AGI',
+    Base: 'Chat With GPT',
     Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'big-AGI',
   },
   Meta: {
-    Description: 'Launch big-AGI to unlock the full potential of AI, with precise control over your data and models. Voice interface, AI personas, advanced features, and fun UX.',
-    SiteName: 'big-AGI | Precision AI for You',
+    Description: 'Launch Chat With GPT to unlock the full potential of AI, with precise control over your data and models. Voice interface, AI personas, advanced features, and fun UX.',
+    SiteName: 'Chat With GPT | Precision AI for You',
     ThemeColor: '#32383E',
     TwitterSite: '@enricoros',
   },
   URIs: {
-    Home: 'https://big-agi.com',
-    // App: 'https://get.big-agi.com',
+    Home: 'https://ChatWithGPT.app',
+    // App: 'ChatWithGPT.app',
     CardImage: 'https://big-agi.com/icons/card-dark-1200.png',
-    OpenRepo: 'https://github.com/enricoros/big-agi',
-    OpenProject: 'https://github.com/users/enricoros/projects/4',
-    SupportInvite: 'https://discord.gg/MkH4qj2Jp9',
-    // Twitter: 'https://www.twitter.com/enricoros',
-    PrivacyPolicy: 'https://big-agi.com/privacy',
+    OpenRepo: '',
+    OpenProject: '',
+    SupportInvite: '',
+    // Twitter: '',
+    PrivacyPolicy: '',
   },
 } as const;
